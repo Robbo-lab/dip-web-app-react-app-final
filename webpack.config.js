9b3a72578ad02882c8783629038e9fc82ed9be47
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2024-10-16 13:59:39 +0800

    chore: working on workflow yml

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.js", // Your entry file
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist"), // Ensure 'dist/' is the output folder
+    clean: true, // Automatically clean old files from dist/
   },
   module: {
     rules: [
@@ -18,23 +20,31 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader",
-        ],
-      }
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: "./public/index.html", // HTML template from public folder
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public"), // Ensure absolute path is used
+          to: ".", // Copy to the root of dist/
+          globOptions: {
+            ignore: ["**/index.html"], // Avoid copying the template twice
+          },
+        },
+      ],
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "dist"), // Serve from 'dist/'
     },
     compress: true,
-    port: 3000,
+    port: 3000, // Development server on port 3000
   },
 };
