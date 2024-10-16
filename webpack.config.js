d37f4d874ac565e4502065b4684aa87416a845be
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2024-10-16 14:10:54 +0800

    fix: yml branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js", // Your entry file
+  entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"), // Ensure 'dist/' is the output folder
-    clean: true, // Automatically clean old files from dist/
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
@@ -26,25 +24,14 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html", // HTML template from public folder
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "public"), // Ensure absolute path is used
-          to: ".", // Copy to the root of dist/
-          globOptions: {
-            ignore: ["**/index.html"], // Avoid copying the template twice
-          },
-        },
-      ],
+      template: "./public/index.html",
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"), // Serve from 'dist/'
+      directory: path.join(__dirname, "dist"),
     },
     compress: true,
-    port: 3000, // Development server on port 3000
+    port: 3000,
   },
 };
